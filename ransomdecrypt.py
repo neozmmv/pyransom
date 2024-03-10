dda3ec14090bff373ed7979149e3017bbfaa5898
neozmmv <83484706+neozmmv@users.noreply.github.com>
2024-03-10 20:54:57 -0300

    Update ransomdecrypt.py

--- ransomdecrypt.py
+++ ransomdecrypt.py
@@ -1,18 +1,18 @@
 import os
 import pyaes
 
-file_name = "JET.png.neozransom"
+file_name = "(encryptedFileName.extension)"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-key = "123456789peste12"
+key = "123456789123456789" #16 digit key (same as encryption)
 aes = pyaes.AESModeOfOperationCTR(key.encode())
 decrypt_data = aes.decrypt(file_data)
 
 os.remove(file_name)
 
-new_file_name = "JET.png"
+new_file_name = "(newDecryptedFileName.extension)"
 new_file = open(new_file_name, "wb")
 new_file.write(decrypt_data)
 new_file.close()
